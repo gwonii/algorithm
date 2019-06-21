dc7502e407a9ed4fb229f326e3c9794cc6f025fe
gwonii <hc.gwonii@gmail.com>
2019-06-21 16:50:27 +0900

    소트인사이드

--- Code/c++/Backjun/TotalCode/1427/Code_1427.cpp
+++ Code/c++/Backjun/TotalCode/1427/Code_1427.cpp
@@ -1,5 +1,31 @@
 //
 // Created by hoho on 2019-06-21.
+//  소트인사이드
 //
 
 #include "Code_1427.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void method_1427(){
+
+    int input;
+
+    cin >> input;
+
+    vector<int> numVector;
+
+    while(input != 0){
+        numVector.push_back(input % 10);
+        input /= 10;
+    }
+
+    sort(numVector.begin(), numVector.end(), greater<int>());
+
+    for(int i : numVector){
+        printf("%d" , i );
+    }
+}
--- Code/c++/Backjun/TotalCode/1427/Code_1427.h
+++ Code/c++/Backjun/TotalCode/1427/Code_1427.h
@@ -5,6 +5,7 @@
 #ifndef TOTALCODE_CODE_1427_H
 #define TOTALCODE_CODE_1427_H
 
+void method_1427();
 
 class Code_1427 {
 
