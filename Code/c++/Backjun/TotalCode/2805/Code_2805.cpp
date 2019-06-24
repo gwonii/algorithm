86ca76f9750b9cbd03316c4163aa06845ca0ebf9
gwonii <hc.gwonii@gmail.com>
2019-06-24 17:51:46 +0900

    나무 자르기

---
+++ Code/c++/Backjun/TotalCode/2805/Code_2805.cpp
@@ -0,0 +1,56 @@
+//
+// Created by hoho on 2019-06-24.
+//
+
+#include "Code_2805.h"
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void mysearch() {
+    
+}
+
+void method_2805() {
+
+    int N, M;
+    int H = 0;
+    vector<int> woods;
+
+    cin >> N;   // 입력할 나무의 개수
+    cin >> M;   // 필요한 나무의 길이
+
+    woods.resize(N);
+    for (int i = 0; i < N; i++) {
+        cin >> woods[i];
+    }
+
+    sort(woods.begin(), woods.end());
+
+    int target = woods[N-1] / 2;
+    int left = 0;
+    int right = woods[N-1];
+    while (target > 0) {
+
+        int A = M;
+        for (int j = N; j > 0; j--) {
+            if (woods[j - 1] > target) {
+                A -= woods[j - 1] - target;
+            }
+        }
+
+        if (A > 0) {
+            H = left;
+            break;
+        }
+        left = target;
+        target = (left + right) / 2;
+    }
+
+
+    printf("%d", H);
+
+}
---
+++ Code/c++/Backjun/TotalCode/2805/Code_2805.h
@@ -0,0 +1,15 @@
+//
+// Created by hoho on 2019-06-24.
+//
+
+#ifndef TOTALCODE_CODE_2805_H
+#define TOTALCODE_CODE_2805_H
+
+void method_2805();
+
+class Code_2805 {
+
+};
+
+
+#endif //TOTALCODE_CODE_2805_H
