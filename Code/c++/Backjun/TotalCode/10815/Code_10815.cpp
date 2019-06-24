37a9896646686ceb2fe671fa58fb69ba90b96cdc
gwonii <hc.gwonii@gmail.com>
2019-06-24 14:06:25 +0900

    숫자 카드

--- Code/c++/Backjun/TotalCode/10815/Code_10815.cpp
+++ Code/c++/Backjun/TotalCode/10815/Code_10815.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "Code_10815.h"
+
+#include <stdio.h>
+#include <vector>
+#include<cstring>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+void method_10815() {
+
+    vector<int> sangeun_card;
+    vector<int> number_card;
+    vector<int> boolean_value;
+
+    int N,M;
+    cin >> N;
+
+    sangeun_card.resize(N);
+    for(int i=0;i<N;i++){
+        cin >> sangeun_card[i];
+    }
+
+    cin >> M;
+    number_card.resize(M);
+    for(int i=0;i<M;i++)
+    {
+        cin >> number_card[i];
+    }
+
+    boolean_value.resize(M);
+
+    sort(sangeun_card.begin(), sangeun_card.end());
+
+    for(int i = 0; i < M; i++){
+
+        printf("%d ",binary_search(sangeun_card.begin(),sangeun_card.end(), number_card[i]));
+
+    }
+}
+
+
--- Code/c++/Backjun/TotalCode/10815/Code_10815.h
+++ Code/c++/Backjun/TotalCode/10815/Code_10815.h
@@ -5,6 +5,7 @@
 #ifndef TOTALCODE_CODE_10815_H
 #define TOTALCODE_CODE_10815_H
 
+void method_10815();
 
 class Code_10815 {
 
