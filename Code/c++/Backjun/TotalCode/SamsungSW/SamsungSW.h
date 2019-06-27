8a8c659896d774b6fb619f1a7af97b4afe317928
gwonii <hc.gwonii@gmail.com>
2019-06-27 11:45:14 +0900

    SamsungSW 헤더파일 변경

--- Code/c++/Backjun/TotalCode/SamsungSW/SamsungSW.h
+++ Code/c++/Backjun/TotalCode/SamsungSW/SamsungSW.h
@@ -5,4 +5,14 @@
 #ifndef TOTALCODE_SAMSUNGSW_H
 #define TOTALCODE_SAMSUNGSW_H
 
+#include "1_math/2609/Code_2609.h"      // 최소공배수와 최대공약수
+#include "1_math/1934/Code_1934.h"      // 최소공배수
+#include "1_math/9613/Code_9613.h"      // 최대공약수의 합
+#include "1_math/1978/Code_1978.h"      // 소수 찾기
+#include "1_math/6588/Code_6588.h"      // 골드바흐의 추측
+
+
+#include "GCD/Code_gcd.h"
+#include "LCM/Code_lcm.h"
+
 #endif //TOTALCODE_SAMSUNGSW_H
