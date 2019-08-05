5b3713a5a31442d4d400d2338b8971ec38fbd53e
gwonii <hc.gwonii@gmail.com>
2019-08-05 18:07:30 +0900

    1406 에디터 풀이 완성

--- Code/c++/Backjun/HappyAlgo/1_dataStructure_base/1406/code_1406.cpp
+++ Code/c++/Backjun/HappyAlgo/1_dataStructure_base/1406/code_1406.cpp
@@ -20,61 +20,123 @@ void method_1406() {
     string str;
     list<char> inputString;
 
-
     cin >> str;
 
     for (int i = 0; i < str.size(); i++) {
         inputString.push_back(str[i]);
     }
 
-    cursor_1406 = str.size();
+    list<char>::iterator iter = inputString.end();  // 현재의 iterator 위치라고 생각하면 됨
+    iter--;
+    list<char>::iterator lastNode = iter;           // 입력해야 할 맨 끝 자리 왼쪽 lastIter로 지정
+    iter++;
 
     cin >> N;
 
-    for (int i = 0; i < N; i++) {
-
-        auto it = inputString.begin();
+    while (N--) {
 
         cin >> action;
 
-        if (action == 'L') {
+        switch (action) {
 
-            if (cursor_1406 != 0)
-                --cursor_1406;
+            case 'L' :
+                if (iter != inputString.begin())
+                    iter--;
+                break;
+            case 'D' :
+                if (iter != inputString.end())
+                    iter++;
+                break;
 
-        } else if (action == 'D') {
+            case 'B' :
+                if (iter != inputString.begin()){
+                    if (iter == inputString.end()) {
+                        inputString.erase(lastNode);
+                        auto tempIter = inputString.end();
+                        lastNode = --tempIter;
+                    } else {
+                        auto remove = --iter;
+                        iter++;
+                        inputString.erase(remove);          // remove하기 전에 iter을 ++ 해줘야 되는데 왜 그럴까?
+                    }
+                }
+                break;
 
-            if (cursor_1406 != inputString.size())
-                ++cursor_1406;
+            case 'P' :
+                char input;
+                cin >> input;
 
-
-        } else if (action == 'B') {
-
-            if (cursor_1406 != 0) {
-
-                for (int j = 0; j < cursor_1406 - 1; j++) {
-                    it++;
+                if(iter == inputString.end()){
+                    inputString.push_back(input);
+                    auto tmpIter = inputString.end();
+                    lastNode = --tmpIter;
+                } else {
+                    inputString.insert(iter, input);
                 }
 
-                inputString.erase(it);
 
-                --cursor_1406;
-            }
 
-        } else if (action == 'P') {
-            char insert;
-            cin >> insert;
+                break;
 
-            for (int j = 0; j < cursor_1406; j++) {
-                it++;
-            }
-
-            inputString.insert(it, insert);
-            ++cursor_1406;
         }
     }
 
-    for (char mm : inputString) {
-        cout << mm;
+    for (char i : inputString) {
+        cout << i;
     }
+
+
+    vector<int> intVec;
+
 }
+
+//cursor_1406 = str.size();
+//
+//    cin >> N;
+//
+//    for (int i = 0; i < N; i++) {
+//
+//        auto it = inputString.begin();
+//
+//        cin >> action;
+//
+//        if (action == 'L') {
+//
+//            if (cursor_1406 != 0)
+//                --cursor_1406;
+//
+//        } else if (action == 'D') {
+//
+//            if (cursor_1406 != inputString.size())
+//                ++cursor_1406;
+//
+//
+//        } else if (action == 'B') {
+//
+//            if (cursor_1406 != 0) {
+//
+//                for (int j = 0; j < cursor_1406 - 1; j++) {
+//                    it++;
+//                }
+//
+//                inputString.erase(it);
+//
+//                --cursor_1406;
+//            }
+//
+//        } else if (action == 'P') {
+//            char insert;
+//            cin >> insert;
+//
+//            for (int j = 0; j < cursor_1406; j++) {
+//                it++;
+//            }
+//
+//            inputString.insert(it, insert);
+//            ++cursor_1406;
+//        }
+//    }
+//
+//    for (char mm : inputString) {
+//        cout << mm;
+//    }
