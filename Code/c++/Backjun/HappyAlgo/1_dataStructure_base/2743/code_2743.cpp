f39bceadb7ef874c425f171ddd6d01720082857e
gwonii <hc.gwonii@gmail.com>
2019-08-19 20:37:49 +0900

    2743번 단어 길이 재기 문제풀이

---
+++ Code/c++/Backjun/HappyAlgo/1_dataStructure_base/2743/code_2743.cpp
@@ -0,0 +1,20 @@
+//
+// Created by hoho on 2019-08-17.
+//
+
+#include "code_2743.h"
+
+#include <iostream>
+
+using namespace std;
+
+
+void method_2743() {
+
+    string inputString;
+
+    cin >> inputString;
+
+    cout << inputString.size();
+
+}
